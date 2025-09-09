d8ab371fc09ce3bdf61ff12485e8f3acbeee0892
Claudio Chies <61051109+Claudio-Chies@users.noreply.github.com>
2025-09-09 08:43:20 +0200

    Change fzf keybindings from zsh to bash
    Updated fzf installation script to use bash keybindings.

--- scripts/ubuntu/fzf.sh
+++ scripts/ubuntu/fzf.sh
@@ -1,4 +1,3 @@
 sudo apt install fzf
 # keybindings for fuzzy history search
-echo "source /usr/share/doc/fzf/examples/key-bindings.zsh" >> ~/.zshrc
-echo "source /usr/share/doc/fzf/examples/completion.zsh" >> ~/.zshrc
+echo "source /usr/share/doc/fzf/examples/key-bindings.bash" >> ~/.bashrc
