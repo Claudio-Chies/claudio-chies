d7a3fd50672cefbce6ae12dd26aa9e4af615df41
Claudio Chies <61051109+Claudio-Chies@users.noreply.github.com>
2026-02-23 14:39:10 +0100

    Update vscode-settings.sh

--- vscode/vscode-settings.sh
+++ vscode/vscode-settings.sh
@@ -1,7 +1,8 @@
 pushd ~/.config/Code/User/
 pushd ~/.config/Code\ -\ OSS/User/
 
-wget -N https://raw.githubusercontent.com/MaEtUgR/MaEtUgR/master/vscode/settings.json
-wget -N https://raw.githubusercontent.com/MaEtUgR/MaEtUgR/master/vscode/keybindings.json
+wget -N https://raw.githubusercontent.com/Claudio-Chies/claudio-chies/master/vscode/settings.json
+wget -N https://raw.githubusercontent.com/Claudio-Chies/claudio-chies/master/vscode/keybindings.json
 
 popd
+
